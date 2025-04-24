ad6117766fc5a0a534e147508ea5d49c348725b3
lipan <670339228@qq.com>
2025-04-24 17:08:01 +0800

    修复代码

--- src/settings.e3bb1.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/GF/StartScene.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"3ae91",internal:"1b082",main:"7ffb6"}};
