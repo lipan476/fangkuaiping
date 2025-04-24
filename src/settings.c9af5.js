ca60a686fe2cdcbdb853de9e6714ae5cf592ea6d
lipan <670339228@qq.com>
2025-04-24 17:14:02 +0800

    asdasdbas

--- src/settings.c9af5.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/GF/StartScene.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"46a45",internal:"1b082",main:"3fd49"}};
